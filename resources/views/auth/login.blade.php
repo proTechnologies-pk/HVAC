95b1333584ae41c3ad839b9e2a236c988bcdc41b
RealSaleem <realsaleem.khi@gmail.com>
2022-02-03 19:08:22 +0500

    auth login logout done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,29 +39,53 @@
                 <span class="r s"></span>
                 <span class="r"></span>
             </div>
-            <div class="card">
-                <div class="card-body text-center">
-                    <div class="mb-4">
-                        <i class="feather icon-unlock auth-icon"></i>
-                    </div>
-                    <h3 class="mb-4">Login</h3>
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
-                    </div>
-                    <div class="input-group mb-4">
-                        <input type="password" class="form-control" placeholder="password">
-                    </div>
-                    {{-- <div class="form-group text-left">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="card">
+                    <div class="card-body text-center">
+                        <div class="mb-4">
+                            <i class="feather icon-unlock auth-icon"></i>
+                        </div>
+                        <h3 class="mb-4">Login</h3>
+                        <div class="input-group mb-3">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="input-group mb-4">
+                            <input id="password" type="password"
+                                class="form-control @error('password') is-invalid @enderror" name="password" required
+                                autocomplete="current-password">
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        {{-- <div class="form-group text-left">
                         <div class="checkbox checkbox-fill d-inline">
                             <input type="checkbox" name="checkbox-fill-1" id="checkbox-fill-a1" checked="">
                             <label for="checkbox-fill-a1" class="cr"> Save Details</label>
                         </div>
                     </div> --}}
-                    <button class="btn btn-primary  mb-4">Login</button>
-                    <p class="mb-2 text-muted">Forgot password? <a href="auth-reset-password.html">Reset</a></p>
-                    {{-- <p class="mb-0 text-muted">Don’t have an account? <a href="auth-signup.html">Signup</a></p> --}}
+                        <button class="btn btn-primary  mb-4" type="submit">Login</button>
+                        @if (Route::has('password.request'))
+                            <p class="mb-2 text-muted">Forgot password?
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    Reset
+
+                                </a>
+                            </p>
+                        @endif
+                        {{-- <p class="mb-0 text-muted">Don’t have an account? <a href="auth-signup.html">Signup</a></p> --}}
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 
