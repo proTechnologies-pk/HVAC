b39e033f48fde0f8a2a766fd151dee4e452a680f
Saleem-Ur-Rehman <78738043+RealSaleem@users.noreply.github.com>
2022-02-03 22:13:41 -0800

    migration updated (#4)
    * updated
    
    * migration updated

---
+++ app/Models/Category.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $table = 'categories';
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    // public function products()
+    // {
+    //     return $this->hasMany('App\Models\Product');//,'id','category_id'
+    // }
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Models\Category','parent_id')->with('parent');
+    }
+
+    public function child()
+    {
+        // recursively return all children
+        return $this->hasOne('App\Models\Category', 'parent_id')->with('child');
+    }
+
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    // This is method where we implement recursive relationship
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id')->with('categories');
+    }
+
+
+}
---
+++ database/migrations/2022_02_03_142034_create_category_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->bigInteger('parent_id');
+            $table->string('slug');
+            $table->boolean('web_display');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
