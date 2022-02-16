f12622c5f7c78b21ecfe6254981b2f9957ef259f
RealSaleem <realsaleem.khi@gmail.com>
2022-02-15 23:19:06 -0800

    view updated

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,3 +1,6 @@
+   <div class="top_bar">
+
+   </div>
    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
        <a href="{{url('/')}}" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
            <h2 class="m-0 text-primary">{{ !is_null(global_setting()) ? global_setting()->title : 'DEMO' }}</h2>
@@ -6,12 +9,10 @@
        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
            <span class="navbar-toggler-icon"></span>
        </button>
-       <div class="collapse navbar-collapse" id="navbarCollapse">
-           <div class="navbar-nav ms-auto p-4 p-lg-0">
-               <a href="index.html" class="nav-item nav-link active">Home</a>
+       <div class="collapse navbar-collapse mr-4" id="navbarCollapse" style="margin-left: 255px;">
+           <div class="navbar-nav  p-4 p-lg-0">
+               <a href="{{url('/')}}" class="nav-item nav-link active">Home</a>
                <a href="about.html" class="nav-item nav-link">About</a>
-
-
                <div class="nav-item dropdown">
                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                    <div class="dropdown-menu fade-down m-0">
@@ -23,7 +24,8 @@
                <a href="contact.html" class="nav-item nav-link">Contact</a>
            </div>
            <div></div>
-           {{-- <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i class="fa fa-arrow-right ms-3"></i></a> --}}
+
        </div>
+
    </nav>
    <!-- Navbar End -->
